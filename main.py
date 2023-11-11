9366b688fa947bf21f1282e39d18025fa8175a52
OverWorld231 <107308919+OverWorld231@users.noreply.github.com>
2023-11-11 19:32:14 +0300

    Поправил отступы

--- main.py
+++ main.py
@@ -7,50 +7,50 @@ from dotenv import load_dotenv
 
 
 def shorten_link(token, link):
-  url = 'https://api-ssl.bitly.com/v4/shorten'
-  params = {"long_url": link}
-  headers = {"Authorization": f"Bearer {token}"}
-
-  response = requests.post(url, headers=headers, json=params)
-  response.raise_for_status()
-  return response.json()["id"]
+    url = 'https://api-ssl.bitly.com/v4/shorten'
+    params = {"long_url": link}
+    headers = {"Authorization": f"Bearer {token}"}
+  
+    response = requests.post(url, headers=headers, json=params)
+    response.raise_for_status()
+    return response.json()["id"]
 
 
 def count_clicks(token, bitlink):
-  url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
-  params = {"unit ": "month", "units": -1}
-  headers = {"Authorization": f"Bearer {token}"}
-  response = requests.get(url, headers=headers, params=params)
-  return response.json()["total_clicks"]
+    url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
+    params = {"unit ": "month", "units": -1}
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()["total_clicks"]
 
 
 def is_bitlink(token, bitlink):
-  url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}"
-  headers = {"Authorization": f"Bearer {token}"}
-  response = requests.get(url, headers=headers)
-  return response.ok
+    url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}"
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers)
+    return response.ok
 
 
 def main():
-  load_dotenv()
-  token = os.environ['BITLY_TOKEN']
-  parser = argparse.ArgumentParser(
-      description=
-      'Это программа позволит вам сократить ссылку или получить количество кликов по сокращенной ссылке'
-  )
-  parser.add_argument('--url', type=str, help='Введите ссылку')
-  args = parser.parse_args()
-  parsed_url = urlparse(args.url)
-  parsed_url = f"{parsed_url.netloc}{parsed_url.path}"
+    load_dotenv()
+    token = os.environ['BITLY_TOKEN']
+    parser = argparse.ArgumentParser(
+        description=
+        'Это программа позволит вам сократить ссылку или получить количество кликов по сокращенной ссылке'
+    )
+    parser.add_argument('--url', type=str, help='Введите ссылку')
+    args = parser.parse_args()
+    parsed_url = urlparse(args.url)
+    parsed_url = f"{parsed_url.netloc}{parsed_url.path}"
 
-  try:
-    if is_bitlink(token, parsed_url):
-      print(count_clicks(token, parsed_url))
-    else:
-      print(shorten_link(token, args.url))
-  except requests.exceptions.HTTPError:
-    print("Ошибка, проверьте ссылку")
+    try:
+        if is_bitlink(token, parsed_url):
+            print(count_clicks(token, parsed_url))
+        else:
+            print(shorten_link(token, args.url))
+    except requests.exceptions.HTTPError:
+        print("Ошибка, проверьте ссылку")
 
 
 if __name__ == "__main__":
-  main()
+    main()
